2db418a51b81bd8ae5fac11f691cd51601b93dee
Amina Donbaayeva <donbaeva.a@bk.ru>
2026-05-01 01:57:40 +0500

    Add TSIS1-4 and practice10-11

--- practice9/moving_ball/ball.py
+++ practice9/moving_ball/ball.py
@@ -1,21 +1,49 @@
 import pygame
+import sys
 
-class Ball:
-    def __init__(self):
-        self.x = 400
-        self.y = 300
-        self.radius = 25
-        self.speed = 20
+pygame.init()
 
-    def move(self, key):
-        if key == pygame.K_LEFT:
-            self.x -= self.speed
-        if key == pygame.K_RIGHT:
-            self.x += self.speed
-        if key == pygame.K_UP:
-            self.y -= self.speed
-        if key == pygame.K_DOWN:
-            self.y += self.speed
+WIDTH, HEIGHT = 600, 400
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Moving Ball")
 
-    def draw(self, screen):
-        pygame.draw.circle(screen, (255,0,0), (self.x,self.y), self.radius)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+
+x, y = WIDTH // 2, HEIGHT // 2
+radius = 25
+speed = 20
+
+running = True
+
+while running:
+    screen.fill(WHITE)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.KEYDOWN:
+
+            if event.key == pygame.K_LEFT:
+                if x - radius - speed >= 0:
+                    x -= speed
+
+            if event.key == pygame.K_RIGHT:
+                if x + radius + speed <= WIDTH:
+                    x += speed
+
+            if event.key == pygame.K_UP:
+                if y - radius - speed >= 0:
+                    y -= speed
+
+            if event.key == pygame.K_DOWN:
+                if y + radius + speed <= HEIGHT:
+                    y += speed
+
+    pygame.draw.circle(screen, RED, (x, y), radius)
+
+    pygame.display.flip()
+
+pygame.quit()
+sys.exit()
